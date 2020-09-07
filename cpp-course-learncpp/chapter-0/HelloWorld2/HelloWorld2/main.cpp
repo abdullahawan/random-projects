198b3cb5a086197ac4b5b8a513db0eb3315ac477
abdullahawan <45406517+abdullahawan@users.noreply.github.com>
2020-09-06 20:30:48 -0400

    Add files via upload

---
+++ cpp-course-learncpp/chapter-0/HelloWorld2/HelloWorld2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld2
+//
+//  Created by Muhammad Awan on 7/23/19.
+//  Copyright © 2019 Muhammad Awan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ cpp-course-learncpp/chapter-1/firstProgram.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter an integere: ";
+    
+    int num{};
+    std::cin >> num;
+    
+    std::cout << "num is " << num << '\n';
+    return 0;
+}
